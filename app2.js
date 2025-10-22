5df4ca43669f21b364a93c93c36459c7bf6f86bc
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2025-10-22 13:26:45 +0200

    fetching category

--- app2.js
+++ app2.js
@@ -2,32 +2,33 @@ const container = document.createElement("div");
 document.body.appendChild(container);
 container.textContent = "container";
 
-
 async function getClothes() {
-try {
-  const url = "https://fakestoreapi.com/products";
-  const res = await fetch(url);
+  try {
+    const url = "https://fakestoreapi.com/products";
+    const res = await fetch(url);
 
-  if (!res.ok) throw Error("ERROOOOR MTH");
-  const data = await res.json();
-  console.log(data);
+    if (!res.ok) throw Error("ERROOOOR MTH");
+    const data = await res.json();
+    console.log(data);
     data.forEach((item) => {
-        const price = document.createElement("p");
-        price.textContent = item.price;
-        container.appendChild(price);
+      const price = document.createElement("p");
+      price.textContent = item.price;
+      container.appendChild(price);
 
-        const description = document.createElement("p");
-        description.textContent = item.description;
-        container.appendChild(description);
+      const description = document.createElement("p");
+      description.textContent = item.description;
+      container.appendChild(description);
+
+      const category = document.createElement("p");
+      category.textContent = item.category;
+      container.appendChild(category);
+
     });
 
-
-
-} catch (err){
+    
+  } catch (err) {
     console.error("feeeel", err.message);
+  }
 }
 
-};
-
 getClothes();
-
