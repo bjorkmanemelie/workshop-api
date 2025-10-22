1507037856993e450ea6a8cde6d8d61ef1c06be1
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2025-10-22 18:25:56 +0200

    Ricky and Morty API training

--- app3.js
+++ app3.js
@@ -2,9 +2,27 @@ const container = document.createElement("div");
 document.body.appendChild(container);
 container.classList.add("products-container");
 
-async function getRecipes() {
+async function getMoM() {
     try { //try betyder att koden försöker köra något som KAN gå fel.
-    const url = 'https://dummyjson.com/recipes'; //fetchar endpointen 
+    const url = "https://rickandmortyapi.com/api"; //fetchar endpointen 
     const res = await fetch (url);
 
+        if (!res.ok) throw Error("Error")
+            const data = await res.json();
+            console.log(data);
+
+        data.forEach((tvShow) => {
+                const productCard = document.createElement("div");
+                productCard.classList.add("product-card");
+
+                const characters = document.createElement("p");
+                container.appendChild(characters);
+            
+
+        });
+    } catch (err) {
+        console.error("nu blev det fel", err.message);
+    }
 }
+
+getMoM();
