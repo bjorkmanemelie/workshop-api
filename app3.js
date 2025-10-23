7d5c43d67401910c377e79edf6d80b528772182c
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2025-10-23 08:23:55 +0200

    felsökning. rätt url, productcard -> container, data.results

--- app3.js
+++ app3.js
@@ -1,28 +1,31 @@
-const container = document.createElement("div"); 
+const container = document.createElement("div");
 document.body.appendChild(container);
 container.classList.add("products-container");
 
 async function getMoM() {
-    try { //try betyder att koden försöker köra något som KAN gå fel.
-    const url = "https://rickandmortyapi.com/api"; //fetchar endpointen 
-    const res = await fetch (url);
+  try {
+    //try betyder att koden försöker köra något som KAN gå fel.
+    const url = "https://rickandmortyapi.com/api/character"; //fetchar endpointen
+    const res = await fetch(url);
 
-        if (!res.ok) throw Error("Error")
-            const data = await res.json();
-            console.log(data);
+    if (!res.ok) throw Error("Error");
+    const data = await res.json();
+    console.log(data);
 
-        data.forEach((tvShow) => {
-                const productCard = document.createElement("div");
-                productCard.classList.add("product-card");
+    data.results.forEach((tvShow) => {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product-card");
 
-                const characters = document.createElement("p");
-                container.appendChild(characters);
-            
+      const characters = document.createElement("p");
+      characters.textContent = tvShow.name;
+      characters.classList.add("product-characters");
 
-        });
-    } catch (err) {
-        console.error("nu blev det fel", err.message);
-    }
+      productCard.appendChild(characters);
+      container.appendChild(productCard);
+    });
+  } catch (err) {
+    console.error("nu blev det fel", err.message);
+  }
 }
 
 getMoM();
